44b6f91f5553a6812ed620f6fbe73607dd3fb0d8
Aleksandar Atanasov <aleks930819@gmail.com>
2023-05-13 17:27:52 +0300

    fixed bug with react native stack

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import Categories from './screens/Categories';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <Categories />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={Categories} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
